04d4e052b0d583c9f4b9e78096bf5e6d93f1c633
Ray Elder <hello@rayelder.com>
2023-12-22 10:42:05 -0800

    Fixed icon size issue

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-// import { config } from '@fortawesome/fontawesome-svg-core'
-// import '@fortawesome/fontawesome-svg-core/styles.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
 // import '../node_modules/@fortawesome/fontawesome-svg-core/styles.css'
-// config.autoAddCss = false
+config.autoAddCss = false
 
 import '../public/styles.css'
 
