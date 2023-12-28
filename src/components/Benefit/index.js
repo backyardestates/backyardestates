3c88237a277f1e3cfdf04c1fc37b8085e45f76f9
Ray Elder <hello@rayelder.com>
2023-12-28 15:57:20 -0800

    Added selected states

--- src/components/Benefit/index.js
+++ src/components/Benefit/index.js
@@ -1,34 +1,63 @@
+import { useState, useRef } from 'react'
 import BenefitTag from '../BenefitTag'
 import style from './Benefit.module.css'
+
+// import { gsap } from 'gsap'
+// import { Flip } from 'gsap/Flip'
+// gsap.registerPlugin(Flip)
 
 export default function Benefit({
     position,
     type = 'Process',
     subtle = false,
+    title,
     children,
 }) {
     let positionStyle = style.b0
 
+    const inputRef = useRef()
+
+    const [selected, setSelected] = useState(false)
+
+    // returns a state object containing data about the elements' current position/size/rotation in the viewport
+    // const start = Flip.getState(inputRef.current)
+
     switch (position) {
         case 0:
-            positionStyle = style.b0
+            if (selected) {
+                positionStyle = style.b0_selected
+            } else {
+                positionStyle = style.b0
+            }
             // type = 'Process'
             break
         case 1:
-            positionStyle = style.b1
-            // type = 'Process'
+            if (selected) {
+                positionStyle = style.b1_selected
+            } else {
+                positionStyle = style.b1
+            }
             break
         case 2:
-            positionStyle = style.b2
-            // type = 'Process'
+            if (selected) {
+                positionStyle = style.b2_selected
+            } else {
+                positionStyle = style.b2
+            }
             break
         case 3:
-            positionStyle = style.b3
-            // type = 'Process'
+            if (selected) {
+                positionStyle = style.b3_selected
+            } else {
+                positionStyle = style.b3
+            }
             break
         case 4:
-            positionStyle = style.b4
-            // type = 'Process'
+            if (selected) {
+                positionStyle = style.b4_selected
+            } else {
+                positionStyle = style.b4
+            }
             break
 
         default:
@@ -36,14 +65,26 @@ export default function Benefit({
             type = 'Process'
     }
 
+    // Flip.from(start, {
+    //     duration: 1,
+    //     ease: 'power1.inOut',
+    //     absolute: true,
+    // })
+
     return (
         <div
+            ref={inputRef}
             className={`${
                 subtle ? style.subtle : style.prominent
             } ${positionStyle}`}
+            onClick={() => setSelected(!selected)}
+            onMouseLeave={() => setSelected(false)}
         >
             <BenefitTag>{type}</BenefitTag>
-            <p>{children}</p>
+            <p className={style.title}>{title}</p>
+            <div className={selected ? style.content_show : style.content_hide}>
+                {children}
+            </div>
         </div>
     )
 }
--- src/components/Benefits/index.js
+++ src/components/Benefits/index.js
@@ -1,4 +1,5 @@
 import Benefit from '../Benefit'
+import StandaloneLink from '../StandaloneLink'
 import style from './Benefits.module.css'
 
 export default function WhyChoose() {
@@ -14,20 +15,142 @@ export default function WhyChoose() {
             </div>
 
             <div className={style.benefits}>
-                <Benefit position={0} type="?">
-                    Effortless experience
+                <Benefit
+                    position={0}
+                    title="Effortless experience"
+                    type="process"
+                >
+                    <p>
+                        <mark>
+                            Tortor posuere ac ut consequat semper viverra nam
+                            libero justo laoreet sit amet cursus sit amet dictum
+                            sit amet justo donec{' '}
+                        </mark>
+                    </p>
+                    <ul className={style.bullets}>
+                        <li>
+                            <mark>One</mark>
+                        </li>
+                        <li>
+                            <mark>Two</mark>
+                        </li>
+                        <li>
+                            <mark>Three</mark>
+                        </li>
+                    </ul>
+                    <StandaloneLink href="/about-us/our-process">
+                        View our process
+                    </StandaloneLink>
                 </Benefit>
-                <Benefit position={1} type="?" subtle>
-                    Superior quality
+                <Benefit
+                    position={1}
+                    title="Superior quality"
+                    type="quality"
+                    subtle
+                >
+                    <p>
+                        <mark>
+                            Tortor posuere ac ut consequat semper viverra nam
+                            libero justo laoreet sit amet cursus sit amet dictum
+                            sit amet justo donec{' '}
+                        </mark>
+                    </p>
+                    <ul className={style.bullets}>
+                        <li>
+                            <mark>One</mark>
+                        </li>
+                        <li>
+                            <mark>Two</mark>
+                        </li>
+                        <li>
+                            <mark>Three</mark>
+                        </li>
+                    </ul>
+                    <StandaloneLink href="/about-us/our-process">
+                        View floor plans
+                    </StandaloneLink>
                 </Benefit>
-                <Benefit position={2} type="Pricing">
-                    Transparent pricing
+                <Benefit
+                    position={2}
+                    title="Transparent pricing"
+                    type="pricing"
+                >
+                    <p>
+                        <mark>
+                            Tortor posuere ac ut consequat semper viverra nam
+                            libero justo laoreet sit amet cursus sit amet dictum
+                            sit amet justo donec{' '}
+                        </mark>
+                    </p>
+                    <ul className={style.bullets}>
+                        <li>
+                            <mark>One</mark>
+                        </li>
+                        <li>
+                            <mark>Two</mark>
+                        </li>
+                        <li>
+                            <mark>Three</mark>
+                        </li>
+                    </ul>
+                    <StandaloneLink href="/about-us/our-process">
+                        View pricing
+                    </StandaloneLink>
                 </Benefit>
-                <Benefit position={3} type="?">
-                    Over 100 years of experience
+                <Benefit
+                    position={3}
+                    title="Over 100 years of combined experience"
+                    type="team"
+                >
+                    <p>
+                        <mark>
+                            Tortor posuere ac ut consequat semper viverra nam
+                            libero justo laoreet sit amet cursus sit amet dictum
+                            sit amet justo
+                        </mark>
+                    </p>
+                    <ul className={style.bullets}>
+                        <li>
+                            <mark>One</mark>
+                        </li>
+                        <li>
+                            <mark>Two</mark>
+                        </li>
+                        <li>
+                            <mark>Three</mark>
+                        </li>
+                    </ul>
+                    <StandaloneLink href="/about-us/our-process">
+                        View team profiles
+                    </StandaloneLink>
                 </Benefit>
-                <Benefit position={4} type="timeline" subtle>
-                    Swift move-in
+                <Benefit
+                    position={4}
+                    title="Swift move-in"
+                    type="timeline"
+                    subtle
+                >
+                    <p>
+                        <mark>
+                            Tortor posuere ac ut consequat semper viverra nam
+                            libero justo laoreet sit amet cursus sit amet dictum
+                            sit amet justo
+                        </mark>
+                    </p>
+                    <ul className={style.bullets}>
+                        <li>
+                            <mark>One</mark>
+                        </li>
+                        <li>
+                            <mark>Two</mark>
+                        </li>
+                        <li>
+                            <mark>Three</mark>
+                        </li>
+                    </ul>
+                    <StandaloneLink href="/about-us/our-process">
+                        View our process
+                    </StandaloneLink>
                 </Benefit>
             </div>
         </div>
