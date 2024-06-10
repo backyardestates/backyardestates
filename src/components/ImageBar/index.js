ab1c7c590498190fe36ce70290e7e778026807f7
Ray Elder <hello@rayelder.com>
2024-06-10 15:24:45 -0700

    Update ImageBar

--- src/components/ImageBar/index.js
+++ src/components/ImageBar/index.js
@@ -1,23 +1,56 @@
+import { useState } from 'react'
+
 import ImageBarButton from '@/components/ImageBarButton'
 import style from './ImageBar.module.css'
 
 export default function ImageBar() {
+    const [room, setRoom] = useState('kitchen')
+
+    function handleClick(rm) {
+        setRoom(rm)
+    }
+
     return (
         <ul className={style.base}>
             <li>
-                <ImageBarButton isSelected />
+                <ImageBarButton
+                    value="kitchen"
+                    handler={handleClick}
+                    room={room}
+                    tooltip="Kitchen"
+                />
             </li>
             <li>
-                <ImageBarButton />
+                <ImageBarButton
+                    value="bathroom"
+                    handler={handleClick}
+                    room={room}
+                    tooltip="Bathroom"
+                />
             </li>
             <li>
-                <ImageBarButton />
+                <ImageBarButton
+                    value="interior"
+                    handler={handleClick}
+                    room={room}
+                    tooltip="Interior features"
+                />
             </li>
             <li>
-                <ImageBarButton />
+                <ImageBarButton
+                    value="exterior"
+                    handler={handleClick}
+                    room={room}
+                    tooltip="Exterior features"
+                />
             </li>
             <li>
-                <ImageBarButton />
+                <ImageBarButton
+                    value="construction"
+                    handler={handleClick}
+                    room={room}
+                    tooltip="Construction specifications"
+                />
             </li>
         </ul>
     )
--- src/components/ImageBarButton/index.js
+++ src/components/ImageBarButton/index.js
@@ -1,10 +1,28 @@
+import { useState } from 'react'
 import Image from 'next/image'
+import Tooltip from '../Tooltip'
 
 import style from './ImageBarButton.module.css'
 
-export default function ImageBarButton({ isSelected = false }) {
+export default function ImageBarButton({ value, room, handler, tooltip }) {
+    const [show, setShow] = useState(false)
+
+    function handleMouseOver() {
+        setShow(true)
+    }
+
+    function handleMouseOut() {
+        setShow(false)
+    }
+
     return (
-        <div className={`${style.base} ${isSelected ? style.selected : ''}`}>
+        <div
+            className={`${style.base} ${room === value ? style.selected : ''}`}
+            onClick={() => handler(value)}
+            onMouseOver={() => handleMouseOver()}
+            onMouseOut={() => handleMouseOut()}
+        >
+            <Tooltip show={show}>{tooltip}</Tooltip>
             <Image
                 src="/images/preview/preview-FPO.png"
                 alt="Picture of the author"
