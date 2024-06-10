f202e2a430ea24bc41227371510f80667f85e061
Ray Elder <hello@rayelder.com>
2024-06-10 15:23:32 -0700

    Update Tooltip

--- src/components/Tooltip/index.js
+++ src/components/Tooltip/index.js
@@ -3,27 +3,19 @@ import style from './Tooltip.module.css'
 export default function Tooltip({ show = false, children }) {
     return (
         <div className={show ? style.tooltip_show : style.tooltip_hide}>
-            <div className={style.content}>{children}</div>
             <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="49"
-                height="24"
-                viewBox="0 0 49 24"
+                width="24"
+                height="48"
+                viewBox="0 0 24 48"
                 fill="none"
-                className={style.knob}
             >
-                <g clipPath="url(#clip0_323_960)">
-                    <path
-                        d="M48.5 0H0.5V12H9.18629C11.308 12 13.3429 12.8429 14.8431 14.3431L23.0858 22.5858C23.8668 23.3668 25.1332 23.3668 25.9142 22.5858L34.1569 14.3431C35.6571 12.8429 37.692 12 39.8137 12H48.5V0Z"
-                        fill="#36484B"
-                    />
-                </g>
-                <defs>
-                    <clipPath id="clip0_323_960">
-                        <rect width="49" height="24" fill="white" />
-                    </clipPath>
-                </defs>
+                <path
+                    d="M24 48L24 0L20 0C15.5817 0 12 3.58172 12 8V8.68629C12 10.808 11.1571 12.8429 9.65685 14.3431L1.41421 22.5858C0.633165 23.3668 0.633165 24.6332 1.41421 25.4142L9.65685 33.6569C11.1571 35.1571 12 37.192 12 39.3137V40C12 44.4183 15.5817 48 20 48H24Z"
+                    fill="#36484B"
+                />
             </svg>
+            <div className={style.content}>{children}</div>
         </div>
     )
 }
