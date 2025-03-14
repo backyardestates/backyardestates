f5f98e5682be29a236bae436a3606c0f10d923cb
Ray Elder <hello@rayelder.com>
2025-03-14 11:02:07 -0600

    Fix Sanity warnings

--- sanity/client.ts
+++ sanity/client.ts
@@ -3,6 +3,6 @@ import { createClient } from 'next-sanity'
 export const client = createClient({
     projectId: '4sw2w31c',
     dataset: 'production',
-    apiVersion: '2024-01-01',
+    apiVersion: '2025-03-14',
     useCdn: false,
 })
--- sanity/live.ts
+++ sanity/live.ts
@@ -1,6 +1,10 @@
 import { defineLive } from 'next-sanity'
 import { client } from '@/sanity/client'
 
+const token = process.env.SANITY_VIEWER_TOKEN
+
 export const { sanityFetch, SanityLive } = defineLive({
-    client: client.withConfig({ apiVersion: 'vX' }),
+    client: client.withConfig({ apiVersion: '2025-03-14' }),
+    serverToken: token,
+    browserToken: token,
 })
