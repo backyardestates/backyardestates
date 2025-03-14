8f3ede9851e51e3b94153163999ea3fc11d3ccba
Ray Elder <hello@rayelder.com>
2025-03-14 14:27:43 -0600

    Complete TestimonalsCarousel component

--- components/Testimonials/index.js
+++ components/Testimonials/index.js
@@ -1,68 +1,19 @@
-// 'use client'
+import { sanityFetch } from '@/sanity/live'
 
-// import { useState, useEffect, useRef } from 'react'
-import ArrowButton from '../ArrowButton'
 import TestimonialsCarousel from '@/components/TestimonialsCarousel'
 import style from './Testimonials.module.css'
 
-export default function Testimonials() {
-    // const [position, setPosition] = useState(1)
-    // const [buttonLeftVisible, setButtonLeftVisible] = useState(false)
-    // const [buttonRightVisible, setButtonRightVisible] = useState(true)
-    // const slidesViewerRef = useRef()
-    // const slidesRef = useRef()
+const STORIES_QUERY = `*[_type == "story" && featured]|order(publishedAt desc){names,portrait,property->{location,floorplan->{name}},slug,quote}`
 
-    function moveLeft() {
-        console.log('move left')
-        // const temp = position - 1
-        // setPosition(temp)
-    }
-
-    function moveRight() {
-        console.log('move right')
-        // const temp = position + 1
-        // setPosition(temp)
-    }
-
-    // useEffect(() => {
-    //     if (count !== 1) {
-    //         switch (position) {
-    //             case 1:
-    //                 setButtonLeftVisible(false)
-    //                 setButtonRightVisible(true)
-    //                 break
-    //             case imageCount:
-    //                 setButtonLeftVisible(true)
-    //                 setButtonRightVisible(false)
-    //                 break
-    //             default:
-    //                 setButtonLeftVisible(true)
-    //                 setButtonRightVisible(true)
-    //         }
-    //     } else {
-    //         setButtonLeftVisible(false)
-    //         setButtonRightVisible(false)
-    //     }
-
-    //     const imageWidth = slidesRef.current.getBoundingClientRect().width
-    //     let xPos = imageWidth * -1 * (position - 1)
-    //     gsap.to(slidesRef.current, {
-    //         x: xPos,
-    //         duration: 0.5,
-    //         ease: 'power2.out',
-    //     })
-    // }, [position, imageCount])
+export default async function Testimonials() {
+    const { data: stories } = await sanityFetch({ query: STORIES_QUERY })
 
     return (
         <div className={style.base}>
             <h2>Trusted by Californian homeowners</h2>
             <p className="small-caps">greater los angeles area</p>
             <div className={style.container}>
-                {/* <ArrowButton direction="left" /> */}
-                <div className={style.spacer} />
-                <TestimonialsCarousel />
-                <div className={style.spacer} />
-                {/* <ArrowButton direction="right" /> */}
+                <TestimonialsCarousel testimonials={stories} />
             </div>
         </div>
     )
--- components/TestimonialsCarousel/index.js
+++ components/TestimonialsCarousel/index.js
@@ -1,20 +1,92 @@
-// 'use server'
+'use client'
 
-import { sanityFetch } from '@/sanity/live'
+import { useState, useEffect, useRef } from 'react'
+import gsap from 'gsap'
+import useViewport from '@/hooks/useViewport'
 
-const STORIES_QUERY = `*[_type == "story" && featured]|order(publishedAt desc){names,portrait,property->{location,floorplan->{name}},slug,quote}`
-
+import ArrowButton from '@/components/ArrowButton'
 import Testimonial from '@/components/Testimonial'
+
 import style from './TestimonialsCarousel.module.css'
 
-export default async function TestimonialsCarousel() {
-    const { data: stories } = await sanityFetch({ query: STORIES_QUERY })
+export default function TestimonialsCarousel({ testimonials }) {
+    const stories = testimonials
+    const slideCount = stories.length
+    const slidesPerView = 1
+    // const slidesToScroll = slideCount - slidesPerView
+    const [position, setPosition] = useState(1)
+    const [buttonLeftVisible, setButtonLeftVisible] = useState(false)
+    const [buttonRightVisible, setButtonRightVisible] = useState(true)
+    const slidesViewerRef = useRef()
+    const slidesRef = useRef()
+    const [isMobile, setIsMobile] = useState(true)
+    const { width } = useViewport()
+
+    function moveLeft() {
+        const temp = position - 1
+        setPosition(temp)
+    }
+
+    function moveRight() {
+        const temp = position + 1
+        setPosition(temp)
+    }
+
+    useEffect(() => {
+        if (width > 768) {
+            setIsMobile(false)
+        }
+
+        switch (position) {
+            case 1:
+                setButtonLeftVisible(false)
+                setButtonRightVisible(true)
+                break
+            case slideCount:
+                setButtonLeftVisible(true)
+                setButtonRightVisible(false)
+                break
+            default:
+                setButtonLeftVisible(true)
+                setButtonRightVisible(true)
+        }
+
+        const imageWidth = slidesRef.current.getBoundingClientRect().width
+        let xPos = (imageWidth / slidesPerView) * -1 * (position - 1)
+        gsap.to(slidesRef.current, {
+            x: xPos,
+            duration: 0.5,
+            ease: 'power2.out',
+        })
+    }, [position, slideCount])
 
     return (
         <div className={style.base}>
-            {stories.map((story, index) => (
-                <Testimonial story={story} key={index} />
-            ))}
+            {!isMobile && (
+                <div className={style.btnLeft}>
+                    {buttonLeftVisible && (
+                        <div onMouseUp={moveLeft}>
+                            <ArrowButton direction="left" />
+                        </div>
+                    )}
+                </div>
+            )}
+            <div ref={slidesViewerRef} className={style.carousel}>
+                <div ref={slidesRef} className={style.slides}>
+                    {stories.map((story, index) => (
+                        <Testimonial story={story} key={index} />
+                    ))}
+                </div>
+            </div>
+            {!isMobile && (
+                <div className={style.btnRight}>
+                    {buttonRightVisible && (
+                        <div onMouseUp={moveRight}>
+                            <ArrowButton direction="right" />
+                        </div>
+                    )}
+                </div>
+            )}
         </div>
     )
 }
