553cc6a562569e726821db9d2a486b2a068aa1a1
Ray Elder <hello@rayelder.com>
2025-03-14 15:27:53 -0600

    Refine VideoPlayer component

--- components/VideoPlayer/index.js
+++ components/VideoPlayer/index.js
@@ -1,34 +1,14 @@
-// 'use client'
-
-// import React, { useEffect, useState } from 'react'
+import { WistiaPlayer } from '@wistia/wistia-player-react'
 import style from './VideoPlayer.module.css'
 
-// import { WistiaProvider, WistiaPlayer } from '@wistia/react-embeds'
-
-import { WistiaPlayer } from '@wistia/wistia-player-react'
-
 export default function VideoPlayer({ wistiaID }) {
-    // const [showComponent, setShowComponent] = useState(false)
-    // console.log('Wistia ID:', typeof wistiaID)
-
-    // useEffect(() => {
-    //     const timeout = setTimeout(() => {
-    //         setShowComponent(true)
-    //         console.log('Wistia component mounted')
-    //         console.log('Bang')
-    //     }, 500)
-    //     return () => clearTimeout(timeout)
-    // }, [])
-
     return (
         <div className={style.base}>
-            {/* <WistiaProvider> */}
             <WistiaPlayer
                 mediaId={wistiaID}
                 playerColor="#405256"
                 aspect={16 / 9}
             />
-            {/* </WistiaProvider> */}
         </div>
     )
 }
