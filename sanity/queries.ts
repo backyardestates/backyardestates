d11e248e69f52df74f45c30e3f3b248aad67d7d7
Ray Elder <hello@rayelder.com>
2025-06-15 10:56:46 -0600

    Remove Custom Estate from table

--- sanity/queries.ts
+++ sanity/queries.ts
@@ -1,1 +1,2 @@
 export const FLOORPLANS_QUERY = `*[_type == "floorplan" && name != "Custom Estate"]|order(orderID asc){name,slug}`
+export const PRICING_FLOORPLANS_QUERY = `*[_type == "floorplan" && name != "Custom Estate"]|order(orderID asc){orderID, isClickable, slug, name, bed, bath, length, width, price}`
