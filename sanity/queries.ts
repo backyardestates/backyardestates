27d4e3889f69ec02114ef2d1382625a490808bd8
Ray Elder <hello@rayelder.com>
2025-07-10 23:12:40 -0600

    Add names to VideoPlayerCarousel

--- sanity/queries.ts
+++ sanity/queries.ts
@@ -1,3 +1,3 @@
 export const FLOORPLANS_QUERY = `*[_type == "floorplan" && name != "Custom Estate"]|order(orderID asc){name,slug}`
 export const PRICING_FLOORPLANS_QUERY = `*[_type == "floorplan" && name != "Custom Estate"]|order(orderID asc){orderID, isClickable, slug, name, bed, bath, length, width, price}`
-export const CUSTOMER_STORIES_QUERY = `*[_type == "story" && featured]|order(publishedAt desc){wistiaId,property->{bed,bath,sqft,floorplan->{name}}}`
+export const CUSTOMER_STORIES_QUERY = `*[_type == "story" && featured]|order(publishedAt desc){wistiaId,names,property->{bed,bath,sqft,floorplan->{name}}}`
